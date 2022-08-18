82cd16a69cfd0eb6f83f79084d7b1a590164c276
turunenv <vili.turunen@aalto.fi>
2022-08-18 15:22:43 +0300

    useState for study-status, rendering Header, MainContent and Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 
+import { useState } from 'react';
+
 function App() {
+  const appStyle = {
+    height: "100vh"
+  }
+
+  const [ studying, setStudying ] = useState(false);
+
+  let buttonLabel = studying ? "Stop study sesh" : "Start studying";
+
   return (
-    <div className="App">
+    <div className="App" style={appStyle}>
       <Header />
       <MainContent />
-      <Footer />
+      <Footer buttonLabel={buttonLabel}/>
     </div>
   );
 }
